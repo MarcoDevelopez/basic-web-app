65d68f0f9706086689fff0fd6ef22f8687f608e6
Marco López <mlopezdev@gmail.com>
2016-09-13 20:51:21 -0500

    Primer envio

---
+++ src/test/java/tutorial/mvc/BlogEntryControllerTest.java
@@ -0,0 +1,38 @@
+package tutorial.mvc;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class BlogEntryControllerTest {
+
+	@InjectMocks
+	private BlogEntryController controller;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+	}
+	
+	@Test
+	public void test() throws Exception {
+		mockMvc.perform(post("/test")
+						.content("{\"title\":\"Test Blog Title\"}")
+						.contentType(MediaType.APPLICATION_JSON)
+				).andExpect(jsonPath("$.title", is("Test Blog Title Wrong")))
+				.andDo(print());
+	}
+
+}
