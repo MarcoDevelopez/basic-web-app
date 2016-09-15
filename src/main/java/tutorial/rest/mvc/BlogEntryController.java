c0a8bfccd348642c08b9a80e62634d614e904e51
Marco López <mlopez@betheltv.pe>
2016-09-15 18:22:29 -0500

    BlogEntryController and Hateoas

---
+++ src/main/java/tutorial/rest/mvc/BlogEntryController.java
@@ -0,0 +1,34 @@
+package tutorial.rest.mvc;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import tutorial.core.entities.BlogEntry;
+import tutorial.core.services.BlogEntryService;
+import tutorial.rest.resources.BlogEntryResource;
+import tutorial.rest.resources.asm.BlogEntryResourceAsm;
+
+@Controller
+@RequestMapping("/rest/blog-entries")
+public class BlogEntryController {
+
+	private BlogEntryService service;
+	
+	public BlogEntryController(BlogEntryService service) {
+		this.service = service;
+	}
+	
+	@RequestMapping(value = "/{blogEntryId}", method = RequestMethod.GET)
+	public ResponseEntity<BlogEntryResource> getBlogEntry(@PathVariable Long blogEntryId) {
+		BlogEntry entry = service.find(blogEntryId);
+		if (entry != null) {
+			BlogEntryResource res = new BlogEntryResourceAsm().toResource(entry);
+			return new ResponseEntity<BlogEntryResource>(res, HttpStatus.OK);
+		}
+		return new ResponseEntity<BlogEntryResource>(HttpStatus.NOT_FOUND);
+	}
+}
--- src/test/java/tutorial/mvc/BlogEntryControllerTest.java
+++ src/test/java/tutorial/mvc/BlogEntryControllerTest.java
@@ -3,36 +3,43 @@ package tutorial.mvc;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import tutorial.core.entities.BlogEntry;
+import tutorial.core.services.BlogEntryService;
+import tutorial.rest.mvc.BlogEntryController;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.Mockito.when;
 
 public class BlogEntryControllerTest {
 
 	@InjectMocks
 	private BlogEntryController controller;
+
+	@Mock
+	private BlogEntryService service;
 	
 	private MockMvc mockMvc;
-	
+
 	@Before
 	public void setup() {
 		MockitoAnnotations.initMocks(this);
 		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
-	
+
 	@Test
-	public void test() throws Exception {
-		mockMvc.perform(post("/test")
-						.content("{\"title\":\"Test Blog Title\"}")
-						.contentType(MediaType.APPLICATION_JSON)
-				).andExpect(jsonPath("$.title", is("Test Blog Title Wrong")))
-				.andDo(print());
+	public void getExistingBlogEntry() throws Exception {
+		when(service.find(1L)).thenReturn(null);
+		
+		mockMvc.perform(get("/rest/blog-entries/1"))
+						.andExpect(status().isNotFound());
+		
 	}
 
 }
